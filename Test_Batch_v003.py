4d4fb9c2c3df6f01d1166168f9ce5de41dae4286
Rakul06 <114053091+Rakul06@users.noreply.github.com>
2024-01-08 11:52:59 +0530

    Create Test_Batch_v003.py

---
+++ Test_Batch_v003.py
@@ -0,0 +1,45 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.bash import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.email_operator import EmailOperator
+from airflow.operators.dagrun_operator import TriggerDagRunOperator
+import requests
+from OverAllTasks import getVersion
+
+default_args = {
+    'owner': 'Rakul',
+    'depends_on_past': False,
+    'start_date': datetime(2024,1,5),
+    'retries': 1,
+    'retry_delay': timedelta(seconds=3),
+    'catchup': False,
+    "email_on_failure":True,
+    "email_on_retry":True,
+    "email":["rahulsk8223@gmail.com"]
+}
+
+dag = DAG(
+    'Test_Batch_v003',
+    default_args=default_args,
+    description='Test batch from batch service',
+    schedule_interval = '09 3 * * *'
+)
+
+email_fun=EmailOperator(
+task_id="Success_email",
+to=('rahulsk8223@gmail.com', ' '),
+subject="Batch Processing success Alert",
+html_content="""<h1>Batch Test_Batch_v003 Executed Successfully !!! <h1>
+""",
+dag = dag
+)
+
+task_1 = PythonOperator(
+    task_id = 'task_1',
+    python_callable = getVersion,
+    dag = dag
+)
+
+task_1 >> email_fun
