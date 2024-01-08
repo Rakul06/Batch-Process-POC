60252a8de50effffe256329f926ae6d9dfd25684
Rakul06 <114053091+Rakul06@users.noreply.github.com>
2024-01-08 11:54:56 +0530

    Update schedule interval for DAG Test_Batch_v003

--- Test_Batch_v003.py
+++ Test_Batch_v003.py
@@ -24,7 +24,7 @@ dag = DAG(
     'Test_Batch_v003',
     default_args=default_args,
     description='Test batch from batch service',
-    schedule_interval = '09 3 * * *'
+    schedule_interval = '0 0 * * *'
 )
 
 email_fun=EmailOperator(
